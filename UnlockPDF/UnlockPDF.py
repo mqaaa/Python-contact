0f7f4d2c62a137e598602662376e330b15d41b08
qmeng <qmeng1128@163.com>
2018-02-11 12:36:34 +0800

    Unlock PDF

---
+++ UnlockPDF/UnlockPDF.py
@@ -0,0 +1,12 @@
+#! python3
+#UnlockPDF.py - 
+#Usage:
+#
+#Author : qmeng
+#MailTo : qmeng1128@163.com
+#QQ     : 1163306125
+#Blog   : http://blog.csdn.net/Mq_Go/
+#Create : 2018-02-11 11:02:32
+#Version: 1.0
+#
+	
